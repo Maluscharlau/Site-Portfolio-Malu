17b39bb6e38960c31d57a48fd4427e1e97ba8850
Bardock <86751142+JorgeSaicoski@users.noreply.github.com>
2022-06-07 15:29:38 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -10,7 +10,7 @@ export default function Home() {
     <div className="bg-bodyColor flex flex-col justify-center">
       <Head>
           <title>Arquiteta Malu</title>
-          <link rel="icon" href="http://localhost:3000/_next/image?url=%2F_next%2Fstatic%2Fmedia%2FlogoHead.801f3bb8.png&w=256&q=75" type="image/icon type"/>
+          <link rel="icon" href="https://maluscharlau.com/_next/image?url=%2F_next%2Fstatic%2Fmedia%2FlogoHead.801f3bb8.png&w=256&q=75" type="image/icon type"/>
           <meta charSet='utf-8' />
           <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
